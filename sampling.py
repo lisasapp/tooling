1fc9750b6efbd299e50ab7dd8fc3f715a47cb9fa
William Wolf <williamabrwolf@gmail.com>
2017-09-20 16:25:19 -0400

    move `log_directory` definition to __init__

--- sampling.py
+++ sampling.py
@@ -25,6 +25,7 @@ class GenerateUniformSampleForClient:
         self.client = self.config['client']
         self.start_date = self.config['start_date']
         self.end_date = self.config['end_date']
+        self.log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
 
     def run(self):
         self._sample_production_logs()
@@ -34,44 +35,40 @@ class GenerateUniformSampleForClient:
         self._print_next_steps()
 
     def _sample_production_logs(self):
-        log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
-        subprocess.run(['cd', log_directory])
+        subprocess.run(['cd', self.log_directory])
         subprocess.run([
             sys.executable,
             os.path.join(ASAPP_PRODML_ROOT, 'tools', 'harvest_cc_logs.py'),
             '--dt_from', self.start_date + 'T0:0:0',
             '--dt_to', self.end_date + 'T0:0:0',
-            '--output', os.path.join(log_directory, 'full-sample.csv'),
+            '--output', os.path.join(self.log_directory, 'full-sample.csv'),
             '--no-collapse'
         ])
 
     def _take_uniform_sample(self):
-        log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
         subprocess.run([
             sys.executable,
             os.path.join(ASAPP_PRODML_ROOT, 'tools', 'hier_sample_logs.py'),
             '--consolidate',
             '--sample-size', '450',
             '--custguid-blacklist', 'comcastblacklist:20170804',
-            'local://' + os.path.join(log_directory, 'full-sample.csv'),
-            os.path.join(log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
+            'local://' + os.path.join(self.log_directory, 'full-sample.csv'),
+            os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
         ])
 
     def _autotag_uniform_sample(self):
-        log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
         subprocess.run([
             sys.executable,
             os.path.join(ASAPP_MLENG_ROOT, 'tools', 'autotagger.py'),
-            '--output-dir', log_directory,
+            '--output-dir', self.log_directory,
             'comcast_baseline,comcast_devtest,comcast_training,ccsrsprodweb',
-            'local://' + os.path.join(log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
+            'local://' + os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
         ])
 
     def _push_uniform_sample_to_s3(self):
-        log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
         subprocess.run([
             'corpora', 'push',
-            '--filepath', os.path.join(log_directory, f'ccsrsprod-week{self.start_date}uniform-450_auto.csv'),
+            '--filepath', os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450_auto.csv'),
             '--bucket', 'asapp-corpora-tagging',
             'condorsrssampling:week{self.start_date}uniform450'
         ])
