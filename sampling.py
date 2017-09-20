425d91a497b5404ab1744fa389880eaa6b124251
William Wolf <williamabrwolf@gmail.com>
2017-09-20 16:26:44 -0400

    make all non-public instance variables private

--- sampling.py
+++ sampling.py
@@ -21,11 +21,11 @@ class GenerateUniformSampleForClient:
 
     def __init__(self, config_path=DEFAULT_CONFIG_PATH):
         config_file = open(config_path, 'rb')
-        self.config = yaml.safe_load(config_file)['sampling']
-        self.client = self.config['client']
-        self.start_date = self.config['start_date']
-        self.end_date = self.config['end_date']
-        self.log_directory = os.path.join(ASAPP_ROOT, 'data', self.client, self.start_date)
+        self._config = yaml.safe_load(config_file)['sampling']
+        self._client = self._config['client']
+        self._start_date = self._config['start_date']
+        self._end_date = self._config['end_date']
+        self._working_directory = os.path.join(ASAPP_ROOT, 'data', self._client, self._start_date)
 
     def run(self):
         self._sample_production_logs()
@@ -35,13 +35,13 @@ class GenerateUniformSampleForClient:
         self._print_next_steps()
 
     def _sample_production_logs(self):
-        subprocess.run(['cd', self.log_directory])
+        subprocess.run(['cd', self._working_directory])
         subprocess.run([
             sys.executable,
             os.path.join(ASAPP_PRODML_ROOT, 'tools', 'harvest_cc_logs.py'),
-            '--dt_from', self.start_date + 'T0:0:0',
-            '--dt_to', self.end_date + 'T0:0:0',
-            '--output', os.path.join(self.log_directory, 'full-sample.csv'),
+            '--dt_from', self._start_date + 'T0:0:0',
+            '--dt_to', self._end_date + 'T0:0:0',
+            '--output', os.path.join(self._working_directory, 'full-sample.csv'),
             '--no-collapse'
         ])
 
@@ -52,34 +52,34 @@ class GenerateUniformSampleForClient:
             '--consolidate',
             '--sample-size', '450',
             '--custguid-blacklist', 'comcastblacklist:20170804',
-            'local://' + os.path.join(self.log_directory, 'full-sample.csv'),
-            os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
+            'local://' + os.path.join(self._working_directory, 'full-sample.csv'),
+            os.path.join(self._working_directory, f'ccsrsprod-week{self._start_date}uniform-450.csv')
         ])
 
     def _autotag_uniform_sample(self):
         subprocess.run([
             sys.executable,
             os.path.join(ASAPP_MLENG_ROOT, 'tools', 'autotagger.py'),
-            '--output-dir', self.log_directory,
+            '--output-dir', self._working_directory,
             'comcast_baseline,comcast_devtest,comcast_training,ccsrsprodweb',
-            'local://' + os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450.csv')
+            'local://' + os.path.join(self._working_directory, f'ccsrsprod-week{self._start_date}uniform-450.csv')
         ])
 
     def _push_uniform_sample_to_s3(self):
         subprocess.run([
             'corpora', 'push',
-            '--filepath', os.path.join(self.log_directory, f'ccsrsprod-week{self.start_date}uniform-450_auto.csv'),
+            '--filepath', os.path.join(self._working_directory, f'ccsrsprod-week{self._start_date}uniform-450_auto.csv'),
             '--bucket', 'asapp-corpora-tagging',
-            'condorsrssampling:week{self.start_date}uniform450'
+            'condorsrssampling:week{self._start_date}uniform450'
         ])
 
     def _print_next_steps(self):
         print(
 f"""
 \nAs a final step, please perform the following:
-    1. Open ccsrsprod-week{self.start_date}uniform-450_auto.csv in Excel.
+    1. Open ccsrsprod-week{self._start_date}uniform-450_auto.csv in Excel.
     2. Remove all columns except: `tag`, `observed`, `weight`, `text`.
-    3. Save result as {self.client}{self.start_date}.xslx.
-    4. Email this file to {self.client.capitalize()}.
+    3. Save result as {self._client}{self._start_date}.xslx.
+    4. Email this file to {self._client.capitalize()}.
 """
         )
