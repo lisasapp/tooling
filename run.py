05cfeae3a58525a1adb15901b329390a91f4ca03
William Wolf <williamabrwolf@gmail.com>
2017-09-21 12:00:41 -0400

    pass correct default config path to `yaml.safe_load`

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ DEFAULT_CONFIG_PATH = 'config.yaml'
 
 
 if __name__ == '__main__':
-    CONFIG = yaml.safe_load(config_file)
+    CONFIG = yaml.safe_load(DEFAULT_CONFIG_PATH)
     GenerateUniformSampleForClient(config=CONFIG).run()
