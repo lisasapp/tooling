53162be5d848a6f30044a65a7a1540d6449c20f3
William Wolf <will@asapp.com>
2017-09-25 17:48:30 -0400

    add `_overwrite_uniform_sample_currently_in_s3` method

--- tools/tagging.py
+++ tools/tagging.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 from tools import ASAPP_ROOT, ASAPP_MLENG_ROOT, ASAPP_PRODML_ROOT
 
@@ -25,9 +25,17 @@ class ProcessTagsThatClientReturns:
         self._run_steps()
 
     def _run_steps(self):
-        pass
+        self._overwrite_uniform_sample_currently_in_s3()
 
     def _validate_input(self):
         input_file_header = set(pd.read_csv(self.input_file).columns)
         if not input_file_header == self.EXPECTED_INPUT_FILE_HEADER:
             raise Exception('Input does not contain the correct columns')
+
+    def _overwrite_uniform_sample_currently_in_s3(self):
+        subprocess.run([
+            'corpora', 'push_update',
+            '--filepath', os.path.join(self._output_directory, f'{self._client}-tagsource{self._start_date}.csv'),
+            '--bucket', 'asapp-corpora-tagging',
+            'condorsrssampling:week{self._start_date}uniform450'
+        ])
