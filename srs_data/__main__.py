a0b295ee736604206df2584f142671cc0e4486fe
William Wolf <will@asapp.com>
2017-09-28 10:38:24 -0400

    run `ProcessTagsThatClientReturns`, and change config filename

--- srs_data/__main__.py
+++ srs_data/__main__.py
@@ -4,11 +4,12 @@ import argparse
 from ruamel import yaml
 
 from asapp.common import cli
-from srs_data.generate_baseline_report import GenerateBaselineReport
 from srs_data.sampling import GenerateUniformSampleForClient
+from srs_data.tagging import ProcessTagsThatClientReturns
+from srs_data.generate_baseline_report import GenerateBaselineReport
 
 
-DEFAULT_CONFIG_PATH = 'srs_data/default_config.yaml'
+DEFAULT_CONFIG_PATH = 'srs_data/config.yaml'
 
 
 def get_parser():
@@ -26,8 +27,9 @@ def run():
     parsed_args = get_parser().parse_args()
     config = parse_config(parsed_args.CONFIG_FILE)
 
-    #GenerateUniformSampleForClient(config).run()
-    GenerateBaselineReport(config).run()
+    # GenerateUniformSampleForClient(config).run()
+    ProcessTagsThatClientReturns(config).run()
+    # GenerateBaselineReport(config).run()
 
 
 if __name__ == '__main__':
