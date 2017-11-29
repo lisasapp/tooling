a8797b1c242db6fc2990b18d229a101344dbe5ef
Lisa Jiang <lisa@asapp.com>
2017-11-29 14:21:35 -0500

    Rename folder. Add some comments

---
+++ srs_data_management/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pythona
+import sys
+import argparse
+from ruamel import yaml
+
+from asapp.common import cli
+from srs_data.sampling import GenerateUniformSampleForClient
+from srs_data.tagging import ProcessTagsThatClientReturns
+from srs_data.generate_baseline_report import GenerateBaselineReport
+
+DEFAULT_CONFIG_PATH = 'srs_data/config.yaml'
+
+
+def get_parser():
+    aparser = cli.ASAPPArgumentParser(description=__doc__)
+    aparser.add_argument('CONFIG_FILE', nargs='?', default=DEFAULT_CONFIG_PATH, type=str, help='Path to the configuration file')
+    return aparser
+
+
+def parse_config(filepath):
+    return yaml.safe_load(open(filepath, 'r'))
+
+
+def run():
+    parsed_args = get_parser().parse_args()
+    config = parse_config(parsed_args.CONFIG_FILE)
+
+    # GenerateUniformSampleForClient(config).run()
+    ProcessTagsThatClientReturns(config).run()
+    # GenerateBaselineReport(config).run()
+
+
+if __name__ == '__main__':
+    sys.exit(run())
