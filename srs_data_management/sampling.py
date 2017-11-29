5a99fbdd2f5be2b0f69c31efe0906d6861819d73
Lisa Jiang <lisa@asapp.com>
2017-11-29 14:35:35 -0500

    remove remaining files in previous repo

--- srs_data_management/sampling.py
+++ srs_data_management/sampling.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 import sys
-from shutil import copyfile
 
 from srs_data.constants import ASAPP_ROOT, ASAPP_PRODML_ROOT, ASAPP_MLENG_ROOT
 from srs_data.base import BaseTool
@@ -28,32 +27,22 @@ class GenerateUniformSampleForClient(BaseTool):
         self._client = config['client']
         self._start_date = config['start_date']
         self._end_date = config['end_date']
-        self._data_count = self._config['data_count']
-        self._output_directory = self._get_output_dir()
+        self._output_directory = os.path.join(ASAPP_ROOT, 'data', self._client, self._start_date)
 
     @property
     def uniform_sample_file(self):
-        return os.path.join(self._output_directory, f'{self._client}srssampling-week{self._start_date}uniform-{self._data_count}.csv')
+        return os.path.join(self._output_directory, f'ccsrsprod-week{self._start_date}uniform-450.csv')
 
     @property
     def autotagged_uniform_sample_file(self):
         return self.uniform_sample_file.replace('.csv', '_auto.csv')
-
-    def _get_output_dir(self):
-        directory = os.path.join(ASAPP_ROOT, 'data', self._client, self._start_date)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        return directory
 
     def _run_steps(self):
         self._sample_production_logs()
         self._take_uniform_sample()
         self._autotag_uniform_sample()
         self._push_uniform_sample_to_s3()
-        if self._client == 'spear':
-            self._create_tagger_shuffle_files()
-            self._push_tagger_directory_to_s3()
-        #self._print_next_steps()
+        self._print_next_steps()
 
     def _validate_input(self):
         """
@@ -63,58 +52,32 @@ class GenerateUniformSampleForClient(BaseTool):
         pass
 
     def _sample_production_logs(self):
-        hostname = ""
-        if self._client == 'condor':
-            hostname = 'comcast-kibana.asapp.com'
-        elif self._client == 'spear':
-            hostname = 'sprint-kibana.asapp.com'
-
         subprocess.run([
             sys.executable,
-            os.path.join(ASAPP_PRODML_ROOT, 'tools', 'harvest_cc_logs.py'),
+            os.path.join(ASAPP_PRODML_ROOT, 'srs_data', 'harvest_cc_logs.py'),
             '--dt_from', self._start_date + 'T0:0:0',
             '--dt_to', self._end_date + 'T0:0:0',
             '--output', os.path.join(self._output_directory, 'full-sample.csv'),
-            '--no-collapse',
-            '--host', hostname
+            '--no-collapse'
         ])
 
     def _take_uniform_sample(self):
-        if self._client == 'condor':
-            subprocess.run([
-                sys.executable,
-                os.path.join(ASAPP_PRODML_ROOT, 'tools', 'hier_sample_logs.py'),
-                '--consolidate',
-                '--sample-size', self._data_count,
-                '--custguid-blacklist', 'comcastblacklist:20170804',
-                'local://' + os.path.join(self._output_directory, 'full-sample.csv'),
-                self.uniform_sample_file
-            ])
-        elif self._client == 'spear':
-            subprocess.run([
-                sys.executable,
-                os.path.join(ASAPP_PRODML_ROOT, 'tools', 'hier_sample_logs.py'),
-                '--consolidate',
-                '--sample-size', self._data_count,
-                '--clean-pii',
-                '--custguid-blacklist', 'spearblacklist:20170822',
-                'local://' + os.path.join(self._output_directory, 'full-sample.csv'),
-                self.uniform_sample_file
-            ])
-
-    def _autotag_uniform_sample(self):
-        datalist = ""
-        if self._client == 'condor':
-            datalist = 'comcast_baseline,comcast_devtest,comcast_training,ccsrsprodweb'
-        elif self._client == 'spear':
-            datalist = 'spear_training,spear_baseline,spear_test'
-
         subprocess.run([
             sys.executable,
-            os.path.join(ASAPP_MLENG_ROOT, 'tools', 'autotagger.py'),
-            '--retag',
+            os.path.join(ASAPP_PRODML_ROOT, 'srs_data', 'hier_sample_logs.py'),
+            '--consolidate',
+            '--sample-size', '450',
+            '--custguid-blacklist', 'comcastblacklist:20170804',
+            'local://' + os.path.join(self._output_directory, 'full-sample.csv'),
+            self.uniform_sample_file
+        ])
+
+    def _autotag_uniform_sample(self):
+        subprocess.run([
+            sys.executable,
+            os.path.join(ASAPP_MLENG_ROOT, 'srs_data', 'autotagger.py'),
             '--output-dir', self._output_directory,
-            datalist,
+            'comcast_baseline,comcast_devtest,comcast_training,ccsrsprodweb',
             'local://' + self.uniform_sample_file
         ])
 
@@ -124,35 +87,16 @@ class GenerateUniformSampleForClient(BaseTool):
             'corpora', 'push',
             '--filepath', self.autotagged_uniform_sample_file,
             '--bucket', 'asapp-corpora-tagging',
-            f'{self._client}srssampling:week{self._start_date}uniform{self._data_count}'
-        ])
-
-    def _create_tagger_shuffle_files(self):
-        tagging_file = os.path.join(self._output_directory, f'{self._client}srstagging-week{self._start_date}uniform{self._data_count}.csv')
-        copyfile(self.autotagged_uniform_sample_file, tagging_file)
-        subprocess.run([
-            sys.executable,
-            os.path.join(ASAPP_MLENG_ROOT, 'tools', 'corpus_split.py'),
-            '--n-splits', self._config['splits'],
-            '--tagger-shuffle', self._config['tagger_count'],
-            '--output-dir', os.path.join(self._output_directory, 'tagger'),
-            'local://' + tagging_file
-        ])
-
-    def _push_tagger_directory_to_s3(self):
-        subprocess.run([
-            'corpora', 'push',
-            '--directory', os.path.join(self._output_directory, 'tagger/'),
-            '--bucket', 'asapp-corpora-tagging'
+            f'condorsrssampling:week{self._start_date}uniform450'
         ])
 
     def _print_next_steps(self):
         print(
-            f"""
-            \nAs a final step, please perform the following:
-                1. Open {self.autotagged_uniform_sample_file} in Excel.
-                2. Remove all columns except: `tag`, `observed`, `weight`, `text`.
-                3. Save result as {CLIENT_FULL_NAMES[self._client]}{self._start_date}.xslx.
-                4. Email this file to {self._client.capitalize()}.
-            """
+f"""
+\nAs a final step, please perform the following:
+    1. Open {self.autotagged_uniform_sample_file} in Excel.
+    2. Remove all columns except: `tag`, `observed`, `weight`, `text`.
+    3. Save result as {CLIENT_FULL_NAMES[self._client]}{self._start_date}.xslx.
+    4. Email this file to {self._client.capitalize()}.
+"""
         )
